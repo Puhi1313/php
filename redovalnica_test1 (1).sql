7fbb56c326e15816c483f4ecd1f4d0b6195c0522
Lumpi-2 <markickajetan@gmail.com>
2025-09-30 09:53:02 +0200

    Add files via upload

---
+++ redovalnica_test1 (1).sql
@@ -0,0 +1,535 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gostitelj: 127.0.0.1
+-- Čas nastanka: 30. sep 2025 ob 09.52
+-- Različica strežnika: 10.4.32-MariaDB
+-- Različica PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Zbirka podatkov: `redovalnica_test1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `gradivo`
+--
+
+CREATE TABLE `gradivo` (
+  `id_gradivo` int(10) UNSIGNED NOT NULL,
+  `id_ucitelj` int(10) UNSIGNED NOT NULL,
+  `id_predmet` int(10) UNSIGNED NOT NULL,
+  `ime_datoteke` varchar(255) NOT NULL,
+  `pot_na_strezniku` varchar(500) NOT NULL,
+  `datum_objave` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `naloga`
+--
+
+CREATE TABLE `naloga` (
+  `id_naloga` int(10) UNSIGNED NOT NULL,
+  `id_ucitelj` int(10) UNSIGNED NOT NULL,
+  `id_predmet` int(10) UNSIGNED NOT NULL,
+  `naslov` varchar(255) NOT NULL,
+  `opis_naloge` text DEFAULT NULL,
+  `rok_oddaje` datetime NOT NULL,
+  `datum_objave` datetime NOT NULL DEFAULT current_timestamp(),
+  `pot_na_strezniku` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Odloži podatke za tabelo `naloga`
+--
+
+INSERT INTO `naloga` (`id_naloga`, `id_ucitelj`, `id_predmet`, `naslov`, `opis_naloge`, `rok_oddaje`, `datum_objave`, `pot_na_strezniku`) VALUES
+(1, 121, 11, 'Statistika', 'Nredi naloge v ucbeniku 4-5', '2025-10-07 00:00:00', '2025-09-30 08:57:42', 'uploads/naloge/1759215462_Vaja 2 - Nup - Kajetan_Markič.docx');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `naloga_ucenec`
+--
+
+CREATE TABLE `naloga_ucenec` (
+  `id_naloga_ucenec` int(10) UNSIGNED NOT NULL,
+  `id_naloga_ucitelj` int(10) UNSIGNED NOT NULL,
+  `id_ucenec` int(10) UNSIGNED NOT NULL,
+  `ime_datoteke` varchar(255) NOT NULL,
+  `pot_na_strezniku` varchar(500) NOT NULL,
+  `datum_oddaje` date NOT NULL,
+  `status_naloge` enum('oddano','sprejeto','zavrnjeno') DEFAULT 'oddano'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `naloga_ucitelj`
+--
+
+CREATE TABLE `naloga_ucitelj` (
+  `id_naloga_ucitelj` int(10) UNSIGNED NOT NULL,
+  `id_ucitelj` int(10) UNSIGNED NOT NULL,
+  `id_predmet` int(10) UNSIGNED NOT NULL,
+  `naslov` varchar(255) NOT NULL,
+  `opis` text DEFAULT NULL,
+  `datum_objave` date NOT NULL,
+  `rok_oddaje` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `oddaja`
+--
+
+CREATE TABLE `oddaja` (
+  `id_oddaja` int(10) UNSIGNED NOT NULL,
+  `id_naloga` int(10) UNSIGNED NOT NULL,
+  `id_ucenec` int(10) UNSIGNED NOT NULL,
+  `datum_oddaje` datetime NOT NULL DEFAULT current_timestamp(),
+  `besedilo_oddaje` text DEFAULT NULL,
+  `pot_na_strezniku` varchar(500) DEFAULT NULL,
+  `status` enum('Oddano','Prepozno','Ocenjeno') NOT NULL DEFAULT 'Oddano',
+  `ocena` varchar(10) DEFAULT NULL,
+  `komentar_ucitelj` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Odloži podatke za tabelo `oddaja`
+--
+
+INSERT INTO `oddaja` (`id_oddaja`, `id_naloga`, `id_ucenec`, `datum_oddaje`, `besedilo_oddaje`, `pot_na_strezniku`, `status`, `ocena`, `komentar_ucitelj`) VALUES
+(1, 1, 142, '2025-09-30 09:50:44', 'resitve:\r\n x = 6', 'uploads/oddaje/142_1_1759217282.docx', 'Oddano', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `predmet`
+--
+
+CREATE TABLE `predmet` (
+  `id_predmet` int(10) UNSIGNED NOT NULL,
+  `ime_predmeta` varchar(100) NOT NULL,
+  `opis` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Odloži podatke za tabelo `predmet`
+--
+
+INSERT INTO `predmet` (`id_predmet`, `ime_predmeta`, `opis`) VALUES
+(11, 'Matematika', 'Matematika in funkcije'),
+(12, 'Slovenščina', 'Jezik in književnost'),
+(13, 'Angleščina', 'Angleški jezik'),
+(14, 'RPR', 'Računalniški praktikum'),
+(15, 'SMV', 'Strojništvo in mehanika'),
+(16, 'NUP', 'Naravoslovni projekti'),
+(17, 'ŠVZ', 'Športna vzgoja');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `ucenec_predmet`
+--
+
+CREATE TABLE `ucenec_predmet` (
+  `id_ucenec` int(10) UNSIGNED NOT NULL,
+  `id_predmet` int(10) UNSIGNED NOT NULL,
+  `id_ucitelj` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Odloži podatke za tabelo `ucenec_predmet`
+--
+
+INSERT INTO `ucenec_predmet` (`id_ucenec`, `id_predmet`, `id_ucitelj`) VALUES
+(135, 11, 121),
+(136, 11, 121),
+(138, 11, 121),
+(141, 11, 121),
+(142, 11, 121),
+(142, 12, 123),
+(135, 12, 124),
+(138, 12, 124),
+(135, 13, 125),
+(136, 13, 125),
+(142, 13, 125),
+(135, 15, 127),
+(135, 16, 127),
+(136, 14, 127),
+(136, 16, 127),
+(136, 15, 129),
+(138, 16, 130),
+(135, 17, 131),
+(138, 17, 131);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `ucenec_urnik`
+--
+
+CREATE TABLE `ucenec_urnik` (
+  `id_ucenec` int(10) UNSIGNED NOT NULL,
+  `id_predmet` int(10) UNSIGNED NOT NULL,
+  `id_ucitelj` int(10) UNSIGNED NOT NULL,
+  `dan` enum('Ponedeljek','Torek','Sreda','Četrtek','Petek') NOT NULL,
+  `ura` tinyint(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `ucitelj_predmet`
+--
+
+CREATE TABLE `ucitelj_predmet` (
+  `id_ucitelj` int(10) UNSIGNED NOT NULL,
+  `id_predmet` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Odloži podatke za tabelo `ucitelj_predmet`
+--
+
+INSERT INTO `ucitelj_predmet` (`id_ucitelj`, `id_predmet`) VALUES
+(121, 11),
+(122, 11),
+(123, 12),
+(124, 12),
+(125, 13),
+(126, 13),
+(127, 14),
+(127, 15),
+(127, 16),
+(128, 14),
+(129, 15),
+(130, 16),
+(131, 17),
+(132, 17);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `uporabnik`
+--
+
+CREATE TABLE `uporabnik` (
+  `id_uporabnik` int(10) UNSIGNED NOT NULL,
+  `ime` varchar(50) NOT NULL,
+  `priimek` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `geslo` varchar(255) NOT NULL,
+  `vloga` enum('admin','ucitelj','ucenec') NOT NULL,
+  `kraj` varchar(100) DEFAULT NULL,
+  `icona_profila` varchar(255) DEFAULT NULL,
+  `prvi_vpis` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Odloži podatke za tabelo `uporabnik`
+--
+
+INSERT INTO `uporabnik` (`id_uporabnik`, `ime`, `priimek`, `email`, `geslo`, `vloga`, `kraj`, `icona_profila`, `prvi_vpis`) VALUES
+(121, 'Helena', 'Viher', 'viher@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(122, 'Tajda', 'Remic', 'remic@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(123, 'Valentina', 'Hrastnik', 'hrastnik@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(124, 'Katja', 'Kolar', 'kolar@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(125, 'Rosana', 'Breznik', 'breznik@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(126, 'Eva', 'Boh', 'boh@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(127, 'Borut', 'Slemenšek', 'slemensek@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(128, 'Jaka', 'Koren', 'koren@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(129, 'Andraž', 'Pušnik', 'pusnik@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(130, 'Boštjan', 'Lubej', 'lubej@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(131, 'Matija', 'Lukner', 'lukner@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(132, 'Sara', 'Padarič', 'padaric@sola.si', 'geslo', 'ucitelj', NULL, NULL, 1),
+(133, 'Ana', 'Novak', 'ana.novak@ucenec.si', 'test123', 'ucenec', NULL, NULL, 0),
+(134, 'Marko', 'Kovač', 'marko.kovac@ucenec.si', 'test123', 'ucenec', NULL, NULL, 1),
+(135, 'Tina', 'Horvat', 'tina.horvat@ucenec.si', 'test123', 'ucenec', NULL, NULL, 0),
+(136, 'Luka', 'Zupan', 'luka.zupan@ucenec.si', 'test123', 'ucenec', NULL, NULL, 0),
+(137, 'Petra', 'Mlakar', 'petra.mlakar@ucenec.si', 'test123', 'ucenec', NULL, NULL, 1),
+(138, 'Nejc', 'Potočnik', 'nejc.potocnik@ucenec.si', 'test123', 'ucenec', NULL, NULL, 0),
+(139, 'Maja', 'Vidmar', 'maja.vidmar@ucenec.si', 'test123', 'ucenec', NULL, NULL, 1),
+(140, 'Jure', 'Bizjak', 'jure.bizjak@ucenec.si', 'test123', 'ucenec', NULL, NULL, 1),
+(141, 'Klara', 'Golob', 'klara.golob@ucenec.si', 'test123', 'ucenec', NULL, NULL, 1),
+(142, 'Žiga', 'Kranjc', 'ziga.kranjc@ucenec.si', 'test123', 'ucenec', NULL, NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabele `urnik`
+--
+
+CREATE TABLE `urnik` (
+  `id_urnik` int(10) UNSIGNED NOT NULL,
+  `id_ucitelj` int(10) UNSIGNED NOT NULL,
+  `id_predmet` int(10) UNSIGNED NOT NULL,
+  `dan` varchar(20) NOT NULL,
+  `ura` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Odloži podatke za tabelo `urnik`
+--
+
+INSERT INTO `urnik` (`id_urnik`, `id_ucitelj`, `id_predmet`, `dan`, `ura`) VALUES
+(13, 121, 11, 'Ponedeljek', 1),
+(14, 124, 12, 'Ponedeljek', 2),
+(15, 128, 14, 'Ponedeljek', 3),
+(16, 131, 17, 'Ponedeljek', 4),
+(17, 125, 13, 'Ponedeljek', 5),
+(18, 130, 16, 'Ponedeljek', 6),
+(19, 122, 11, 'Torek', 1),
+(20, 123, 12, 'Torek', 2),
+(21, 129, 15, 'Torek', 3),
+(22, 126, 13, 'Torek', 4),
+(23, 132, 17, 'Torek', 5),
+(24, 127, 14, 'Torek', 6),
+(25, 121, 11, 'Sreda', 1),
+(26, 125, 13, 'Sreda', 2),
+(27, 123, 12, 'Sreda', 3),
+(28, 127, 16, 'Sreda', 4),
+(29, 127, 15, 'Sreda', 5),
+(30, 131, 17, 'Sreda', 6),
+(31, 124, 12, 'Četrtek', 1),
+(32, 126, 13, 'Četrtek', 2),
+(33, 122, 11, 'Četrtek', 3),
+(34, 128, 14, 'Četrtek', 4),
+(35, 129, 15, 'Četrtek', 5),
+(36, 132, 17, 'Četrtek', 6),
+(37, 121, 11, 'Petek', 1),
+(38, 123, 12, 'Petek', 2),
+(39, 125, 13, 'Petek', 3),
+(40, 127, 14, 'Petek', 4),
+(41, 130, 16, 'Petek', 5),
+(42, 131, 17, 'Petek', 6);
+
+--
+-- Indeksi zavrženih tabel
+--
+
+--
+-- Indeksi tabele `gradivo`
+--
+ALTER TABLE `gradivo`
+  ADD PRIMARY KEY (`id_gradivo`),
+  ADD KEY `id_ucitelj` (`id_ucitelj`),
+  ADD KEY `id_predmet` (`id_predmet`);
+
+--
+-- Indeksi tabele `naloga`
+--
+ALTER TABLE `naloga`
+  ADD PRIMARY KEY (`id_naloga`),
+  ADD KEY `id_ucitelj` (`id_ucitelj`),
+  ADD KEY `id_predmet` (`id_predmet`);
+
+--
+-- Indeksi tabele `naloga_ucenec`
+--
+ALTER TABLE `naloga_ucenec`
+  ADD PRIMARY KEY (`id_naloga_ucenec`),
+  ADD KEY `id_naloga_ucitelj` (`id_naloga_ucitelj`),
+  ADD KEY `id_ucenec` (`id_ucenec`);
+
+--
+-- Indeksi tabele `naloga_ucitelj`
+--
+ALTER TABLE `naloga_ucitelj`
+  ADD PRIMARY KEY (`id_naloga_ucitelj`),
+  ADD KEY `id_ucitelj` (`id_ucitelj`),
+  ADD KEY `id_predmet` (`id_predmet`);
+
+--
+-- Indeksi tabele `oddaja`
+--
+ALTER TABLE `oddaja`
+  ADD PRIMARY KEY (`id_oddaja`),
+  ADD UNIQUE KEY `idx_naloga_ucenec` (`id_naloga`,`id_ucenec`),
+  ADD KEY `id_ucenec` (`id_ucenec`);
+
+--
+-- Indeksi tabele `predmet`
+--
+ALTER TABLE `predmet`
+  ADD PRIMARY KEY (`id_predmet`);
+
+--
+-- Indeksi tabele `ucenec_predmet`
+--
+ALTER TABLE `ucenec_predmet`
+  ADD PRIMARY KEY (`id_ucenec`,`id_predmet`),
+  ADD KEY `id_predmet` (`id_predmet`),
+  ADD KEY `fk_ucitelj` (`id_ucitelj`);
+
+--
+-- Indeksi tabele `ucenec_urnik`
+--
+ALTER TABLE `ucenec_urnik`
+  ADD PRIMARY KEY (`id_ucenec`,`id_predmet`,`id_ucitelj`,`dan`,`ura`),
+  ADD KEY `id_predmet` (`id_predmet`),
+  ADD KEY `id_ucitelj` (`id_ucitelj`);
+
+--
+-- Indeksi tabele `ucitelj_predmet`
+--
+ALTER TABLE `ucitelj_predmet`
+  ADD PRIMARY KEY (`id_ucitelj`,`id_predmet`),
+  ADD KEY `id_predmet` (`id_predmet`);
+
+--
+-- Indeksi tabele `uporabnik`
+--
+ALTER TABLE `uporabnik`
+  ADD PRIMARY KEY (`id_uporabnik`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indeksi tabele `urnik`
+--
+ALTER TABLE `urnik`
+  ADD PRIMARY KEY (`id_urnik`),
+  ADD KEY `id_ucitelj` (`id_ucitelj`),
+  ADD KEY `id_predmet` (`id_predmet`);
+
+--
+-- AUTO_INCREMENT zavrženih tabel
+--
+
+--
+-- AUTO_INCREMENT tabele `gradivo`
+--
+ALTER TABLE `gradivo`
+  MODIFY `id_gradivo` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT tabele `naloga`
+--
+ALTER TABLE `naloga`
+  MODIFY `id_naloga` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT tabele `naloga_ucenec`
+--
+ALTER TABLE `naloga_ucenec`
+  MODIFY `id_naloga_ucenec` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT tabele `naloga_ucitelj`
+--
+ALTER TABLE `naloga_ucitelj`
+  MODIFY `id_naloga_ucitelj` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT tabele `oddaja`
+--
+ALTER TABLE `oddaja`
+  MODIFY `id_oddaja` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT tabele `predmet`
+--
+ALTER TABLE `predmet`
+  MODIFY `id_predmet` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT tabele `uporabnik`
+--
+ALTER TABLE `uporabnik`
+  MODIFY `id_uporabnik` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=143;
+
+--
+-- AUTO_INCREMENT tabele `urnik`
+--
+ALTER TABLE `urnik`
+  MODIFY `id_urnik` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- Omejitve tabel za povzetek stanja
+--
+
+--
+-- Omejitve za tabelo `gradivo`
+--
+ALTER TABLE `gradivo`
+  ADD CONSTRAINT `gradivo_ibfk_1` FOREIGN KEY (`id_ucitelj`) REFERENCES `uporabnik` (`id_uporabnik`),
+  ADD CONSTRAINT `gradivo_ibfk_2` FOREIGN KEY (`id_predmet`) REFERENCES `predmet` (`id_predmet`);
+
+--
+-- Omejitve za tabelo `naloga`
+--
+ALTER TABLE `naloga`
+  ADD CONSTRAINT `naloga_ibfk_1` FOREIGN KEY (`id_ucitelj`) REFERENCES `uporabnik` (`id_uporabnik`),
+  ADD CONSTRAINT `naloga_ibfk_2` FOREIGN KEY (`id_predmet`) REFERENCES `predmet` (`id_predmet`);
+
+--
+-- Omejitve za tabelo `naloga_ucenec`
+--
+ALTER TABLE `naloga_ucenec`
+  ADD CONSTRAINT `naloga_ucenec_ibfk_1` FOREIGN KEY (`id_naloga_ucitelj`) REFERENCES `naloga_ucitelj` (`id_naloga_ucitelj`),
+  ADD CONSTRAINT `naloga_ucenec_ibfk_2` FOREIGN KEY (`id_ucenec`) REFERENCES `uporabnik` (`id_uporabnik`);
+
+--
+-- Omejitve za tabelo `naloga_ucitelj`
+--
+ALTER TABLE `naloga_ucitelj`
+  ADD CONSTRAINT `naloga_ucitelj_ibfk_1` FOREIGN KEY (`id_ucitelj`) REFERENCES `uporabnik` (`id_uporabnik`),
+  ADD CONSTRAINT `naloga_ucitelj_ibfk_2` FOREIGN KEY (`id_predmet`) REFERENCES `predmet` (`id_predmet`);
+
+--
+-- Omejitve za tabelo `oddaja`
+--
+ALTER TABLE `oddaja`
+  ADD CONSTRAINT `oddaja_ibfk_1` FOREIGN KEY (`id_naloga`) REFERENCES `naloga` (`id_naloga`),
+  ADD CONSTRAINT `oddaja_ibfk_2` FOREIGN KEY (`id_ucenec`) REFERENCES `uporabnik` (`id_uporabnik`);
+
+--
+-- Omejitve za tabelo `ucenec_predmet`
+--
+ALTER TABLE `ucenec_predmet`
+  ADD CONSTRAINT `fk_ucitelj` FOREIGN KEY (`id_ucitelj`) REFERENCES `uporabnik` (`id_uporabnik`),
+  ADD CONSTRAINT `ucenec_predmet_ibfk_1` FOREIGN KEY (`id_ucenec`) REFERENCES `uporabnik` (`id_uporabnik`),
+  ADD CONSTRAINT `ucenec_predmet_ibfk_2` FOREIGN KEY (`id_predmet`) REFERENCES `predmet` (`id_predmet`);
+
+--
+-- Omejitve za tabelo `ucenec_urnik`
+--
+ALTER TABLE `ucenec_urnik`
+  ADD CONSTRAINT `ucenec_urnik_ibfk_1` FOREIGN KEY (`id_ucenec`) REFERENCES `uporabnik` (`id_uporabnik`),
+  ADD CONSTRAINT `ucenec_urnik_ibfk_2` FOREIGN KEY (`id_predmet`) REFERENCES `predmet` (`id_predmet`),
+  ADD CONSTRAINT `ucenec_urnik_ibfk_3` FOREIGN KEY (`id_ucitelj`) REFERENCES `uporabnik` (`id_uporabnik`);
+
+--
+-- Omejitve za tabelo `ucitelj_predmet`
+--
+ALTER TABLE `ucitelj_predmet`
+  ADD CONSTRAINT `ucitelj_predmet_ibfk_1` FOREIGN KEY (`id_ucitelj`) REFERENCES `uporabnik` (`id_uporabnik`),
+  ADD CONSTRAINT `ucitelj_predmet_ibfk_2` FOREIGN KEY (`id_predmet`) REFERENCES `predmet` (`id_predmet`);
+
+--
+-- Omejitve za tabelo `urnik`
+--
+ALTER TABLE `urnik`
+  ADD CONSTRAINT `urnik_ibfk_1` FOREIGN KEY (`id_ucitelj`) REFERENCES `uporabnik` (`id_uporabnik`),
+  ADD CONSTRAINT `urnik_ibfk_2` FOREIGN KEY (`id_predmet`) REFERENCES `predmet` (`id_predmet`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
